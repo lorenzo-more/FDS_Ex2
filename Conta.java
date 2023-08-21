33bcaf1b4e7eeaad40bd69830255dd3bfa92d5bf
Mateus Poletto <pole@pop-os.localdomain>
2023-08-21 18:38:34 -0300

    Força kkk

---
+++ Conta.java
@@ -0,0 +1,3 @@
+public class Conta {
+    public Conta(Long numero, String nomeCorrentista, int saldoLivreInicial, int salAplicacaoInicial, double taxaRemuneracao, double taxaSaldoNegativo, Categoria categoriaInicial){ }
+}
